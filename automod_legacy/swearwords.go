071e5eb474552848e13c623ee68f2fc293a3f8d2
Apni412 <71573102+Apni412@users.noreply.github.com>
2020-09-20 06:41:15 +0700

    indonesian harassment word
    add some indonesian bad word

--- automod_legacy/swearwords.go
+++ automod_legacy/swearwords.go
@@ -75,4 +75,9 @@ var BuiltinSwearWords = map[string]bool{
 	"asshat":      true,
 	"pu55y":       true,
 	"pen1s":       true,
+	"kontol":      true,
+	 "memek":      true,
+	"jembut":      true,
+	"kont0l":      true,
+	"k0nt0l":      true,
 }
